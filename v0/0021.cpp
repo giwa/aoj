909c6f2f2606f4bdb20656738c19b7e7f653e0df
giwa <ugw.gi.world@gmail.com>
2015-03-14 09:07:59 +0000

    0023

--- v0/0021.cpp
+++ v0/0021.cpp
@@ -3,16 +3,14 @@ using namespace std;
 
 int main(){
     double x1, y1, x2, y2, x3, y3, x4, y4, ab, cd;
-    int n;
+    int n, r;
 
     cin >> n;
     while(n--){
         cin >>x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4;
 
-        ab = (y1 - y2) / (x1 - x2);
-        cd = (y3 - y4) / (x3 - x4);
-
-        if ((y2 - y1)/ (x2 - x1) == (y4 - y3) / (x4 - x3)){
+        r = (x2 - x1) * (y3 - y4) - (x3 -x4) * (y2 - y1);
+        if (!r){
             cout << "YES" << endl;
         }else{
             cout << "NO" << endl;
